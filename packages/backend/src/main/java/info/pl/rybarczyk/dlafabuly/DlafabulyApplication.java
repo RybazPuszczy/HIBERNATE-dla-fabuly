e695e93bac4aca3929409d3af26d22d02a656e13
RybazPuszczy <75949266+RybazPuszczy@users.noreply.github.com>
2025-06-23 11:12:49 +0200

    Add Spring Initializr initialized files

---
+++ packages/backend/src/main/java/info/pl/rybarczyk/dlafabuly/DlafabulyApplication.java
@@ -0,0 +1,13 @@
+package info.pl.rybarczyk.dlafabuly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlafabulyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlafabulyApplication.class, args);
+	}
+
+}
---
+++ packages/backend/src/test/java/info/pl/rybarczyk/dlafabuly/DlafabulyApplicationTests.java
@@ -0,0 +1,13 @@
+package info.pl.rybarczyk.dlafabuly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlafabulyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
